81b4e065e17ec56ab8e0e2feb4629f67730aaf73
Jackson Kasi <109948871+jacksonkasi1@users.noreply.github.com>
2026-08-09 00:55:19 +0530

    Fix BoxScope match parent modifier import

--- app/src/main/java/com/jacksonkasi/cliplex/ui/components/ClipLexComponents.kt
+++ app/src/main/java/com/jacksonkasi/cliplex/ui/components/ClipLexComponents.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -34,7 +33,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.Role
